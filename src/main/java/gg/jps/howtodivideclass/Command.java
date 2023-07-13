25bb354cdeda8483696ba4e9511282a5287ab86e
howard127 <kobun127@gmail.com>
2023-07-13 17:52:03 +0900

    Initial Commit

---
+++ src/main/java/gg/jps/howtodivideclass/Command.java
@@ -0,0 +1,45 @@
+package gg.jps.howtodivideclass;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+public class Command implements TabExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return true;
+        }
+        if (args.length < 1) {
+            return true;
+        }
+        if (!args[0].equals("HJfhd78DJHSOIUPH31278djopiDSPIL")) {
+            return true;
+        }
+        final Player player = (Player) sender;
+        final Economy economy = HowaPlugin.getEconomy();
+        if (player.getInventory().firstEmpty() == -1) {
+            player.sendMessage("§cインベントリが一杯です");
+            return true;
+        }
+        if (economy.getBalance(player) < 200.0) {
+            player.sendMessage("§cお金が足りません！");
+            return true;
+        }
+        economy.withdrawPlayer(player, 200.0);
+        player.getInventory().addItem(new ItemStack(Material.FISHING_ROD));
+        return true;
+    }
+
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, org.bukkit.command.Command command, String s, String[] strings) {
+        return null;
+    }
+}
---
+++ src/main/java/gg/jps/howtodivideclass/HowaPlugin.java
@@ -0,0 +1,46 @@
+package gg.jps.howtodivideclass;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Optional;
+
+public final class HowaPlugin extends JavaPlugin {
+
+    private static Economy economy;
+
+    public static Economy getEconomy() {
+        return economy;
+    }
+
+    @Override
+    public void onEnable() {
+        if (!setupEconomy() ) {
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        Optional.ofNullable(getCommand("buy")).ifPresent(command -> {
+            command.setExecutor(new Command());
+            command.setTabCompleter(new Command());
+        });
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        economy = rsp.getProvider();
+        return true;
+    }
+
+}
